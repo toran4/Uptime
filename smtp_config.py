9c8c5e75acc8e757afb8b69e8273e5935936419b
Tor <toran4@users.noreply.github.com>
2022-04-22 16:30:59 +0200

    config in env

--- smtp_config.py
+++ smtp_config.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
+import os
 
-# Sender email, smtp user and password for account sending alerts
-sender = "monitor@example.com"
-user = "exampleusername"
-password = "examplepassword"
+# Login email and password for account sending alerts
+_sender = "monitor@example.com"
+_user = "exampleuser"
+_password = "examplepassword"
+
+sender = os.environ.get("EMAIL_SENDER", _sender)
+user = os.environ.get("SMTP_USER", _user)
+password = os.environ.get("SMTP_PASSWORD", _password)
 
 # Host and port for smtp server sending alerts
-host = "mail.example.com"
-port = 587
+_host = "mail.example.com"
+_port = "587"
+
+host = os.environ.get("SMTP_HOST", _host)
+port = int(os.environ.get("SMTP_PORT", _port))
 
 # Recipients that will get alert
-receivers = ['john.doe@gmail.com']
+_receivers = "john.doe@gmail.com"
+
+receivers = list1 = [i.strip() for i in os.environ.get("EMAIL_RECEIVERS", _receivers).split(",")] 
